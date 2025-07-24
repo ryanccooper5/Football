d5b3e6d0856152c8dc658cc8da8179cb6bbcb329
Ryan Cooper <ryanccooper@mac.com>
2025-07-24 14:17:02 -0700

    Initial Commit

---
+++ Football/FootballApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootballApp.swift
+//  Football
+//
+//  Created by Ryan Cooper on 7/24/25.
+//
+
+import SwiftUI
+
+@main
+struct FootballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
